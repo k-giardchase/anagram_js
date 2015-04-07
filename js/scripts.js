852e6207a2cc56c75d0c2b9579d93b06232cf7cb
Kyle Giard-Chase & David Eliason <student@epicodus.com>
2015-04-07 16:39:48 -0700

    Add html to take user input and display result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,65 @@
-var anagram = function(input_word, possible_anagram)
+var anagram = function(input_word, possible_anagrams)
 {
     var split_input_word = input_word.split("").sort().join("");
     var output_anagrams = [];
 
-    var split_possible_anagram = possible_anagram.split(" ");
-      for(var i = 0; i<split_possible_anagram.length;i++)
+    var one_possible_anagram = possible_anagrams.split(" ");
+      for(var i = 0; i<one_possible_anagram.length;i++)
       {
-          var split_word = split_possible_anagram[i].split("").sort().join("");
+          var anagram_word = one_possible_anagram[i];
+          var anagram_guinea_pig = anagram_word.split("").sort().join("");
 
 
-          if(split_input_word === split_word)
+          if(anagram_guinea_pig.indexOf(split_input_word) !== -1)
           {
-              output_anagrams.push(true);
+              output_anagrams.push(anagram_word);
           }
-          else
-           {
-              output_anagrams.push(false);
-           }
       }
     return output_anagrams;
 };
+
+$(document).ready(function(){
+    $('form#anagram_form').submit(function(event){
+        var comparison_word = $("input#original_word").val();
+        var anagram_choices = $("input#comparison_words").val();
+
+        var result = anagram(comparison_word, anagram_choices);
+
+        $(".anagram_outputs").empty();
+        $("#comparison_word").empty();
+
+        for(var i = 0; i < result.length; i++) {
+            $("ul.anagram_outputs").prepend('<li>'+ result[i] + '</li>');
+        }
+        $("#comparison_word").text(comparison_word);
+        $(".results").show().fadeIn(3000);
+        event.preventDefault();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+<form role="form">
+    <div class="form-group">
+        <label for="original_word">Enter word to compare: </label>
+        <input name="original_word" id="original_word" type="text" placeholder="coffee"></input>
+    </div>
+    <div class="form-group">
+        <label for="comparison_words">Enter comparison words: </label>
+        <input name="comparison_words" id="comparison_words" type="text" placeholder="toffee"></input>
+    </div>
+<button class="btn btn-primary btn-xs">Click</button>
+</form>
+
+*/
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
 describe("anagram", function(){
     it("return the single combination of the word 'a'", function(){
-        expect(anagram("a", "a")).to.eql([true]);
+        expect(anagram("a", "a")).to.eql(['a']);
     })
 
     it("returns the anagram match for a two-character word", function() {
-        expect(anagram("an", "an")).to.eql([true]);
+        expect(anagram("an", "an")).to.eql(['an']);
     })
 
     it("returns the anagram matches with possible anagram matches to input",function(){
-        expect(anagram("eat","tea eta")).to.eql([true,true]);
+        expect(anagram("eat","tea eta")).to.eql(['tea','eta']);
     })
 
     it("returns the anagram matches with possible anagram matches to input",function(){
-        expect(anagram("eat","tza eta")).to.eql([false,true]);
+        expect(anagram("eat","tza eta")).to.eql(['eta']);
     })
 });
